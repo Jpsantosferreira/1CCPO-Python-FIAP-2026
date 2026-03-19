68e83fb97caecb6291e9d16c9ded5009e5d78508
Jpsantosferreira <jpsferreirajp@gmail.com>
2026-03-19 20:57:09 -0300

    aula 2 variaveis e operadores.

---
+++ aula02-variaveis-operadores/aula02a-desafios.py
@@ -0,0 +1,22 @@
+'''Desafios:
+    desafio 1
+    desafio 2
+    desafio 3
+'''
+# Desafio 1
+nome = input("Digite seu nome: ")
+
+print(f"Seja bem-vindo, {nome}")
+
+# Desafio 2
+idade = int(input("Digite o ano em que você nasceu: "))
+mes = int(input("Digite o mês em que vc nasceu: "))
+dia = int(input("Digite o dia do seu aniversário: "))
+
+print(f"Data do seu nascimento: {idade, mes, dia}")
+
+#Desafio 3
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Agora, digite outro número: "))
+
+print(f"A soma dos dois números: {numero1 + numero2}")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,37 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # concatenação de strings
+
+# comentários de 1 linha
+
+'''
+    comentários de
+    multiplas 
+    linhas
+'''
+
+
+# variáveis
+nome = "Alexandre" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+print(f"Olá, {nome}!!!")
+
+# INPUT - Simulação de forms no CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nascimento = 1999
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"Sua idade é: {idade}")
+
+
