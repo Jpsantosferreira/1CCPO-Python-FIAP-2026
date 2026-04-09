d7b37913ea890c095e58e21f0dcc5b271cd4993b
Jpsantosferreira <jpsferreirajp@gmail.com>
2026-04-09 20:58:37 -0300

    aula03 - modulos, funcoes, condicionais, operadores logicos

---
+++ aula02-variaveis-operadores/aula02b-operadores.py
@@ -0,0 +1,44 @@
+num1 = 4
+num2 = 2
+
+print(type(num1), type(num2))
+
+operacao = num1 / num2
+print(operacao, type(operacao))
+
+# OPERADOR DE ATRIBUIÇÃO
+num = 15
+print() #pular uma linha
+print(num)
+
+num = num + 2
+print(num)
+
+num *= 2  #operação + atribuição!
+print(num)
+
+# OPERADORES RELACIONAIS
+print()
+print(6 != 5)  # ==(igual)...!=(diferente)
+
+idade = 21
+print(idade >= 21)
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade)
+
+# STRING
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1.upper() == nome2.upper())
+
+#gustavo guanabara - canal YouTube
+
+
+
+
+
---
+++ aula03-modulos-condicionais/aula03c-condicionais.py
@@ -0,0 +1,23 @@
+'''
+nota_final = 6
+
+if nota_final < 6:
+    print("Reprovado")
+else:
+    print("Aprovado")
+
+print("FIM")
+'''
+# ESTRUTURA CONDICIONAL ENCADEADA
+
+nota_final = 3.0
+
+if nota_final < 4:
+    print("Reprovado")
+
+elif nota_final < 6:
+    print("Recuperação")
+else:
+    print("Aprovado")
+
+print("FIM")
