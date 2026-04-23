1f09cc6c46275fdd29bd8bf3478baa09ceb7e070
Jpsantosferreira <jpsferreirajp@gmail.com>
2026-04-23 20:52:28 -0300

    aula04-while_for

---
+++ aula04-estruturas-repeticao/aula04-atividade02.py
@@ -0,0 +1,36 @@
+# def verificar_nota(nota):
+#     while nota < 0 or nota > 10:
+#         print("A nota deve estar entre 0 e 10")
+#         nota = float(input("Digite a nota novamente: "))
+#     return nota
+#
+#
+# notaA = float(input("Digite a primeira nota: "))
+# while notaA < 0 or notaA > 10:         # usa 'or' pq se usar 'e', nn existe um numero menor que 0 e maior que 10 ao mesmo tempo, erro logico
+#     print("A nota deve estar entre 0 e 10")
+#     notaA = float(input("Digite a nota novamente: "))
+#
+# notaB = float(input("Digite a segunda nota: "))
+# while notaB < 0 or notaB > 10:
+#     print("A nota deve estar entre 0 e 10")
+#     notaB = float(input("Digite a nota novamente: "))
+#
+# media = (notaA + notaB) / 2
+# print(media)
+#
+
+def verificar_nota(nota):
+    while nota < 0 or nota > 10:
+        print("A nota deve estar entre 0 e 10")
+        nota = float(input("Digite a nota novamente: "))
+    return nota
+
+
+notaA = float(input("Digite a primeira nota: "))
+notaA = verificar_nota(notaA)
+
+notaB = float(input("Digite a segunda nota: "))
+notaB = verificar_nota(notaB)
+
+media = (notaA + notaB) / 2
+print(media)
---
+++ aula04-estruturas-repeticao/aula04b-for.py
@@ -0,0 +1,22 @@
+# for c_msc in range(3):
+#     print(f"Música {c_msc}")
+#
+# # de 1 até 12, pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+
+#  ATIVIDADE 3
+# qtd_musicas = int(input("Quantas músicas vc tem na playlist? (db): "))
+#
+# for i in range(qtd_musicas):
+#     print(f"Música {i}")
+
+
+# laços aninhados
+# rep. encadeada
+
+for i in range(0, 4 ):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
+
+
