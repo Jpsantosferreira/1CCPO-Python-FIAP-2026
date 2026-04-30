02ee8a4b03a42f874b794e494eb056591872a3c9
Jpsantosferreira <jpsferreirajp@gmail.com>
2026-04-30 20:49:35 -0300

    aula-string-arrays-matrizes

---
+++ aula05-strings-arrays-matrizes/aula05b-arrays.py
@@ -0,0 +1,18 @@
+lista_frutas = ["Laranja", "Uva", "Melão"]
+
+# print(lista_frutas[0]) = "Laranja"
+# print(lista_frutas[1]) = "Uva"
+# print(lista_frutas[2]) = "Melão"
+
+lista_frutas.append("Maçã")
+print(lista_frutas)
+#lista_frutas[3] = "Maçã"
+
+for i in range(len(lista_frutas)):
+    print(lista_frutas[i])
+
+print()
+
+for fruta in lista_frutas:
+    print(fruta)
+
